436aa0dd250b4b06cbd4e17a1dadaffab8098eca
imsfcj <107828099+imsfcj@users.noreply.github.com>
2022-11-23 16:50:42 -0500

    Update CJ 查单.py

--- pages/CJ 查单.py
+++ pages/CJ 查单.py
@@ -105,16 +105,18 @@ if bt1 :
     x = 0
     #url_head = 'https://map.cluster.uniexpress.org/map/getorderdetail?tno='
     for line in h :
-        if len(line) < 3 : continue
+        if len(line) < 3 :
+            cllm1.write(line)
+            continue
         headers = {
             'authority': 'map.cluster.uniexpress.org',
             'accept': 'application/json',
-            'accept-language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
+            'accept-language': 'en-US,en;q=0.9',
             'access-control-allow-credentials': 'true',
             'access-control-allow-headers': 'Origin, X-Requested-With, Content-Type, Accept, origin, content-type, accept',
             'access-control-allow-methods': 'GET, PUT, POST, DELETE, OPTIONS',
             'access-control-allow-origin': '*',
-            'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbWFwLmNsdXN0ZXIudW5pZXhwcmVzcy5vcmcvbWFwL2xvZ2luIiwiaWF0IjoxNjY5MDkzMzQ0LCJleHAiOjE2NjkxNzk3NDQsIm5iZiI6MTY2OTA5MzM0NCwianRpIjoidGI5RVZobmRGUXNKeVNmbCIsInN1YiI6MjE3LCJwcnYiOiJlOGNmNTQ2ZTZiNTNmMmIxOWY3ZTQ1OWJkMzEyZjcxMTQwODkxMzllIiwiaWQiOjIxNywibW9kZWwiOiJlY3NfY3NfYWNjb3VudCIsInJvbGVzIjpbIkRyaXZlciBBZG1pbmlzdHJhdG9yIiwiSW5jaWRlbnQgTWFuYWdlbWVudCJdLCJ1aV9hYmlsaXRpZXMiOlsxLDIsMyw0LDUsNiw3LDgsOSwxMCwxMSwxMiwxMywxNSwxNiwxNywxOCwxOSwyMiwyMywzMDEsMzAzLDMwMiwzMDQsMzIsMjcsMzFdLCJ3YXJlaG91c2UiOlsiMiIsIjIwIiwiMTAiLCI5Il0sInVzX2ZsYWciOmZhbHNlfQ.mYsEWO_TU9JfMTq8BRa18GmvWYW7v8txJAYqCqX47L0',
+            'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbWFwLmNsdXN0ZXIudW5pZXhwcmVzcy5vcmcvbWFwL2xvZ2luIiwiaWF0IjoxNjY5MjM5NTQ3LCJleHAiOjE2NjkzMjU5NDcsIm5iZiI6MTY2OTIzOTU0NywianRpIjoibURFWjk2blM5VloySnF5cSIsInN1YiI6MjE3LCJwcnYiOiJlOGNmNTQ2ZTZiNTNmMmIxOWY3ZTQ1OWJkMzEyZjcxMTQwODkxMzllIiwiaWQiOjIxNywibW9kZWwiOiJlY3NfY3NfYWNjb3VudCIsInJvbGVzIjpbIkRyaXZlciBBZG1pbmlzdHJhdG9yIiwiSW5jaWRlbnQgTWFuYWdlbWVudCJdLCJ1aV9hYmlsaXRpZXMiOlsxLDIsMyw0LDUsNiw3LDgsOSwxMCwxMSwxMiwxMywxNSwxNiwxNywxOCwxOSwyMiwyMywzMDEsMzAzLDMwMiwzMDQsMzIsMjcsMzFdLCJ3YXJlaG91c2UiOlsiMiIsIjIwIiwiMTAiLCI5IiwiMSIsIjQiLCI3IiwiNSIsIjE0IiwiMTciLCIyNCIsIjIxIiwiMyIsIjYiLCIxMiIsIjE5IiwiMTUiLCIyMiIsIjI1IiwiMTMiLCIxNiIsIjIzIiwiMjYiLCIxMSIsIjgiXSwidXNfZmxhZyI6ZmFsc2V9.twE-pRncSt95dIxLgqQpCIf_GxmOLTvYK4ATCr0JOiI',
             'origin': 'https://unimap.cluster.uniexpress.org',
             'referer': 'https://unimap.cluster.uniexpress.org/',
             'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
@@ -125,6 +127,7 @@ if bt1 :
             'sec-fetch-site': 'same-site',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
         }
+
         #x = 'MBORDU7012624903'
         params = {
             'tno': line,
