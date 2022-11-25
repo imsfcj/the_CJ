28385f952b037c151a24eb15019d50f88239ad91
imsfcj <107828099+imsfcj@users.noreply.github.com>
2022-11-25 16:31:04 -0500

    Update CJ 查单.py

--- pages/CJ 查单.py
+++ pages/CJ 查单.py
@@ -96,6 +96,40 @@ cllm5.subheader("更新时间")
 cllm6.subheader("映射")
 cll1,cll2,cll3,cll4,cll5,cll6 = st.columns(6)
 
+headers = {
+    'authority': 'map.cluster.uniexpress.org',
+    'accept': 'application/json',
+    'accept-language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
+    'access-control-allow-credentials': 'true',
+    'access-control-allow-headers': 'Origin, X-Requested-With, Content-Type, Accept, origin, content-type, accept',
+    'access-control-allow-methods': 'GET, PUT, POST, DELETE, OPTIONS',
+    'access-control-allow-origin': '*',
+    'authorization': 'Bearer null',
+    'content-type': 'application/json;charset=UTF-8',
+    'origin': 'https://unimap.cluster.uniexpress.org',
+    'referer': 'https://unimap.cluster.uniexpress.org/',
+    'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-site',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
+}
+
+params = {
+    'username': 'jason_cj',
+}
+
+json_data = {
+    'password': '16988',
+}
+
+responsee = requests.post('https://map.cluster.uniexpress.org/map/login', params=params, headers=headers, json=json_data)
+jss = json.loads(responsee.content)
+
+the_pas = 'Bearer ' +  jss['data']['token']
+
 if bt1 :
     st.sidebar.write('\n -----     准备中     ----- ')
     my_bar = main_container.progress(0)
@@ -116,7 +150,7 @@ if bt1 :
             'access-control-allow-headers': 'Origin, X-Requested-With, Content-Type, Accept, origin, content-type, accept',
             'access-control-allow-methods': 'GET, PUT, POST, DELETE, OPTIONS',
             'access-control-allow-origin': '*',
-            'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbWFwLmNsdXN0ZXIudW5pZXhwcmVzcy5vcmcvbWFwL2xvZ2luIiwiaWF0IjoxNjY5MjM5NTQ3LCJleHAiOjE2NjkzMjU5NDcsIm5iZiI6MTY2OTIzOTU0NywianRpIjoibURFWjk2blM5VloySnF5cSIsInN1YiI6MjE3LCJwcnYiOiJlOGNmNTQ2ZTZiNTNmMmIxOWY3ZTQ1OWJkMzEyZjcxMTQwODkxMzllIiwiaWQiOjIxNywibW9kZWwiOiJlY3NfY3NfYWNjb3VudCIsInJvbGVzIjpbIkRyaXZlciBBZG1pbmlzdHJhdG9yIiwiSW5jaWRlbnQgTWFuYWdlbWVudCJdLCJ1aV9hYmlsaXRpZXMiOlsxLDIsMyw0LDUsNiw3LDgsOSwxMCwxMSwxMiwxMywxNSwxNiwxNywxOCwxOSwyMiwyMywzMDEsMzAzLDMwMiwzMDQsMzIsMjcsMzFdLCJ3YXJlaG91c2UiOlsiMiIsIjIwIiwiMTAiLCI5IiwiMSIsIjQiLCI3IiwiNSIsIjE0IiwiMTciLCIyNCIsIjIxIiwiMyIsIjYiLCIxMiIsIjE5IiwiMTUiLCIyMiIsIjI1IiwiMTMiLCIxNiIsIjIzIiwiMjYiLCIxMSIsIjgiXSwidXNfZmxhZyI6ZmFsc2V9.twE-pRncSt95dIxLgqQpCIf_GxmOLTvYK4ATCr0JOiI',
+            'authorization': the_pas,
             'origin': 'https://unimap.cluster.uniexpress.org',
             'referer': 'https://unimap.cluster.uniexpress.org/',
             'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
