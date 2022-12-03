68cf3925341f1a85e4141fac5a81ee8399691349
imsfcj <107828099+imsfcj@users.noreply.github.com>
2022-12-03 03:17:46 -0500

    Update CJ_count.py

--- pages/CJ_count.py
+++ pages/CJ_count.py
@@ -13,8 +13,15 @@ credentials = service_account.Credentials.from_service_account_info(
 )
 conn = connect(credentials=credentials)
 
-# Perform SQL query on the Google Sheet.
-# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
 
+sheet_url = st.secrets["private_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
-st.write(conn)
+# Print results.
+for row in rows:
+    st.write(f"{row.Driver} has a :{row.Area}:")
