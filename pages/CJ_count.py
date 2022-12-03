d0695ee1af971ee80c9b1ea35440ceac386895b3
imsfcj <107828099+imsfcj@users.noreply.github.com>
2022-12-03 03:19:53 -0500

    Update CJ_count.py

--- pages/CJ_count.py
+++ pages/CJ_count.py
@@ -22,6 +22,7 @@ def run_query(query):
 sheet_url = st.secrets["private_gsheets_url"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
-# Print results.
+alist = {}
 for row in rows:
-    st.write(f"{row.Driver} has a :{row.Area}:")
+    alist[row[0]] = row[1]
+st.write(alist)
