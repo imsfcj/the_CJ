579f1378b8e5d20487295165394339282c54ccd8
imsfcj <107828099+imsfcj@users.noreply.github.com>
2022-12-03 02:21:51 -0500

    Create CJ_count.py

---
+++ pages/CJ_count.py
@@ -0,0 +1,20 @@
+# streamlit_app.py
+
+import streamlit as st
+from google.oauth2 import service_account
+from gsheetsdb import connect
+
+# Create a connection object.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+
+st.write(conn)
