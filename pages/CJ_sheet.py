b6f2475f9b94a1b00c0e174c008f900541acafe4
imsfcj <107828099+imsfcj@users.noreply.github.com>
2022-12-06 04:38:15 -0500

    Update CJ_sheet.py

--- pages/CJ_sheet.py
+++ pages/CJ_sheet.py
@@ -1,6 +1,6 @@
 import streamlit as st 
 from pandas import DataFrame
-from gspread_pandas import Spread,Client
+from gspread_pandas import Client
 from google.oauth2 import service_account
 
 scope = ['https://spreadsheets.google.com/feeds',
@@ -10,15 +10,14 @@ credentials = service_account.Credentials.from_service_account_info(
                 st.secrets["gcp_service_account"], scopes = scope)
 client = Client(scope=scope,creds=credentials)
 spreadsheetname = "司机一周统计表"
-spread = Spread(spreadsheetname,client = client)
+spreadsheet = client.open(spreadsheetname)
 
 # Check the connection
-st.write(spread.url)
+st.write(spreadsheet.url)
 
 the_new = st.button('new')
 if the_new :
     old_title = "template"
     new_title = "the_new_sheet"
     index = 1  # Insert the new sheet at index 1
-    spread.duplicate_sheet(old_title, new_title, index)
-
+    client.duplicate_sheet(spreadsheet, old_title, new_title, index)
