ae68ce5e3dbbbb4d51284c3be1b6d6176c4cfa7b
imsfcj <107828099+imsfcj@users.noreply.github.com>
2022-12-06 17:13:51 -0500

    Update CJ_sheet.py

--- pages/CJ_sheet.py
+++ pages/CJ_sheet.py
@@ -98,7 +98,7 @@ spreadsheetname = "司机一周统计表"
 spread = Spread(spreadsheetname,client = client)
 sh = client.open(spreadsheetname)
 schedule_sheet = sh.worksheet(this_week)
-df = DataFrame(schedule_sheet.get_all_records(head=['Driver','Location',the_pick]))
+df = schedlue_sheet.sheet_to_df(columns=['Driver', 'Location', the_pick])
 day_driver = df.to_dict()
 st.write(day_driver)
 
