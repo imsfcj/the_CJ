b9c2716468d0e3481555c22532410ea6bea9bc79
imsfcj <107828099+imsfcj@users.noreply.github.com>
2022-12-06 04:50:19 -0500

    Update CJ_sheet.py

--- pages/CJ_sheet.py
+++ pages/CJ_sheet.py
@@ -22,5 +22,6 @@ if the_new :
     old_title = "template"
     new_title = "the_new_sheet"
     index = 1  # Insert the new sheet at index 1
-    worksheet = sh.get_worksheet(old_title)
-    sh.duplicate_sheet(worksheet.id, new_title, index)
+    worksheet = sh.worksheet(old_title)
+    worksheet_id = worksheet.id
+    sh.duplicate_sheet(worksheet_id, new_title, index)
