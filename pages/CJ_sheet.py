73ffcebba33efaad17fd95fd1ebede3996bfebee
imsfcj <107828099+imsfcj@users.noreply.github.com>
2022-12-06 04:42:06 -0500

    Update CJ_sheet.py

--- pages/CJ_sheet.py
+++ pages/CJ_sheet.py
@@ -10,14 +10,16 @@ credentials = service_account.Credentials.from_service_account_info(
                 st.secrets["gcp_service_account"], scopes = scope)
 client = Client(scope=scope,creds=credentials)
 spreadsheetname = "司机一周统计表"
-spreadsheet = client.open(spreadsheetname)
+spread = Spread(spreadsheetname,client = client)
 
 # Check the connection
 st.write(spreadsheet.url)
+
+sh = client.open(spreadsheetname)
 
 the_new = st.button('new')
 if the_new :
     old_title = "template"
     new_title = "the_new_sheet"
     index = 1  # Insert the new sheet at index 1
-    client.copy_sheet(spreadsheet, old_title, new_title, index)
+    sh.duplicate_sheet(old_title, new_title, index)
