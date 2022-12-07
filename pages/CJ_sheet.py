07b86cf3881d7ff31c1b13af3925479192b32460
imsfcj <107828099+imsfcj@users.noreply.github.com>
2022-12-06 20:10:00 -0500

    Update CJ_sheet.py

--- pages/CJ_sheet.py
+++ pages/CJ_sheet.py
@@ -111,7 +111,7 @@ for index, row in day_driver.iterrows():
         count[row['Location']] = []
         count[row['Location']].append(on_board)
 
-df = pd.DataFrame.from_dict(count, columns=count.keys())
+df = pd.DataFrame.from_dict(count,orient='columns', columns=count.keys())
 st.write(df)
 
 
