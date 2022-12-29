dd9336ee12f52e1a53ec6df88b5c5aba33cbcb73
imsfcj <107828099+imsfcj@users.noreply.github.com>
2022-12-28 21:42:51 -0500

    Update CJ 时效.py

--- pages/CJ 时效.py
+++ pages/CJ 时效.py
@@ -47,10 +47,11 @@ if bt1 :
         if 'DELIVERED' not in jss['data'][driver].keys():
             zero_list = zero_list + str(driver) + '/'
             fail_list = fail_list + str(driver) + '/'
+            st.write(str(driver),'0%')
             continue
         d_percent = jss['data'][driver]['DELIVERED'] / jss['data'][driver]['total']
         if d_percent < 0.9 :
-            #st.write(driver,d_percent)
+            st.write(driver,d_percent)
             fail_list = fail_list + str(driver) + '/'
 
         #st.subheader(driver)
